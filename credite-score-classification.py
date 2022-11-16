626dd8454d442d08747252890c72ed7bfdc73d73
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-16 17:51:30 +0700

    Update credite-score-classification.py

--- credite-score-classification.py
+++ credite-score-classification.py
@@ -1,15 +1,9 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
 st.write(""" 
 # Aplikasi Web Klasifikasi Credit Score
 Oleh Okhi Sahrul Barkah
 """)
-
-dataset = "https://gist.github.com/khikisb/7f53f136bf6dd98639fc3042af46fbdc"
-data = pd.read_csv(dataset)
-
 
 st.write("======================================================================")
 nama_nasabah = st.text_input('Masukkan Nama Nasabah')
