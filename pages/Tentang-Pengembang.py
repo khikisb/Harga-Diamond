25a3a278047d44bd8f8e88fc17e6bb2e41aca1a8
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-18 21:43:45 +0700

    Update and rename Tentang-Pengembang to Tentang-Pengembang.py

--- pages/Tentang-Pengembang.py
+++ pages/Tentang-Pengembang.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+
+st.set_page_config(
+    page_title="Tentang Pengembang",
+    page_icon="👋",
+)
+
 st.title("Tentang Pengembang")
 
 st.write("Nama saya Okhi Sahrul Barkah")
