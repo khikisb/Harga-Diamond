8c37f6c5521fa8ac0aff1f31a8b741cfc1ff00ce
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-18 18:54:22 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -61,18 +61,17 @@ def predict(carat, cut, color, clarity, depth, table, x, y, z):
     return prediction
 
 
-st.title('Diamond Price Predictor')
-st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the characteristics of the diamond:')
-carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
-cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
-clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-depth = st.number_input('Diamond Depth Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+st.title('Prediksi Harga Diamond')
+st.header('Masukkan Kriteria Diamond :')
+carat = st.number_input('Berat Karat:', min_value=0.1, max_value=10.0, value=1.0)
+cut = st.selectbox('Nilai Diamond:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
+color = st.selectbox('Nilai Warna:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
+clarity = st.selectbox('Nilai Kejelasan Warna', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
+depth = st.number_input('Persentase Kedalaman Berlian:', min_value=0.1, max_value=100.0, value=1.0)
+table = st.number_input('Persentase Tabel Berlian:', min_value=0.1, max_value=100.0, value=1.0)
+x = st.number_input('Panjang Berlian (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+y = st.number_input('Lebar Berlian (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+z = st.number_input('Tinggi Berlian (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 
 if st.button('Predict Price'):
     price = predict(carat, cut, color, clarity, depth, table, x, y, z)
