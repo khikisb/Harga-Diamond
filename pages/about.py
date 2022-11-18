fa7c0d38ff508834dbcc7982637bee3758451b51
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-18 21:16:13 +0700

    Add files via upload

---
+++ pages/about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('About')
+st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.header('Perkenalkan nama saya Okhi Sahrul Barkah')
+st.header('NIM: 210411100112')
