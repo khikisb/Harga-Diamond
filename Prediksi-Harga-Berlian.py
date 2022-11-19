4cac68546a739f8e227d146e1a3c5f0cbd2a631c
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-19 17:25:41 +0700

    Update Prediksi-Harga-Berlian.py

--- Prediksi-Harga-Berlian.py
+++ Prediksi-Harga-Berlian.py
@@ -84,8 +84,7 @@ z = st.number_input('Tinggi Berlian (Z) dalam mm:', min_value=0.1, max_value=100
 
 if st.button('Prediksi Harga Belian'):
     price = predict(carat, cut, color, clarity, depth, table, x, y, z)
-    rupiah = price * 15.000
     st.success(f'Harga Berlian Tersebut adalah ${price[0]:.2f} USD')
-    st.success(f'Harga Berlian Tersebut adalah ${rupiah[0]:.2f} IDR')
+
     
 
