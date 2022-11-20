3cbc43abae9df6bb54ee9bd1d1a8e89bd2d05bc3
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-20 14:12:46 +0700

    Update Code-Project.py

--- pages/Code-Project.py
+++ pages/Code-Project.py
@@ -11,10 +11,5 @@ st.title("Halo Semuanya !👋")
 
 st.write('Kamu Bisa menemukan Source Code Project ini')
 
-
-if st.button('Di Github'):
-    js = "window.open('https://github.com/khikisb/Harga-Diamond/')"  # New tab or window
-    js = "window.location.href = 'https://github.com/khikisb/'"  # Current tab
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    st.bokeh_chart(div)
+link = '[GitHub](https://github.com/khikisb/Harga-Diamond/)'
+st.markdown(link, unsafe_allow_html=True)
